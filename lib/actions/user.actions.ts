19209de88bd2042d9beaad5154022320f6f99ef2
Jake Toomay <81326775+jtoomay@users.noreply.github.com>
2023-09-06 10:20:11 -0500

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,23 +10,23 @@ export async function getUser() {
   const { firstName, lastName, username } = (await currentUser()) || {} // get the name and username from the current clerk user, if there is none, create an empty object
   try {
     const user = await User.findOne({ id: clerkUser.userId }) // try to find this user in our DB using the user's name from clerk
-    if (user) return user
+    if (user) return user // return the user if we find them
 
-    return await User.create({ id: clerkUser.userId, firstName: firstName, lastName: lastName, username: username })
+    return await User.create({ id: clerkUser.userId, firstName: firstName, lastName: lastName, username: username }) // if not, create one with these props
   } catch (err: any) {
     console.log("Error Message: ", err)
   }
 }
-interface UserUpdateOptions {
+interface UserUpdateOptions { // these are the props that can be used to update the user.
   bio?: string
   username?: string
 }
 
 export async function updateUser(options: UserUpdateOptions) {
-  connectToDB()
-  const clerkUser = auth()
+  connectToDB() // connect to the DB
+  const clerkUser = auth() // get the user from Clerk
   try {
-    await User.findOneAndUpdate({ id: clerkUser.userId }, { $set: options }, { upsert: true })
+    await User.findOneAndUpdate({ id: clerkUser.userId }, { $set: options }, { upsert: true }) // Find and update the user with the clerk user id and set the other options that we pass in. 
   } catch (err) {
     console.log("Error Message: ", err)
   }
