37f00a7b9055757c57fda35975a521c86a53a591
Jake Toomay <81326775+jtoomay@users.noreply.github.com>
2023-09-06 10:03:42 -0500

    Added Comments to the connectToDB function

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose"
 let isConnected = false
 
 export const connectToDB = async () => {
-  mongoose.set("strictQuery", true)
+  //Mongoose will only allow query filters to match fields that are defined in the schema. 
+  //If a query filter matches a field that is not defined in the schema, Mongoose will throw an error.
+  mongoose.set("strictQuery", true) 
 
-  if (!process.env.MONGODB_URL) return console.log("Missing URL")
-  if (isConnected) return console.log("Already Connected")
+  if (!process.env.MONGODB_URL) return console.log("Missing URL") // If you dont have a mongoDB url in the .env, log that theres a missing url
+  if (isConnected) return console.log("Already Connected") // If it is already connected when we call this, dont try to connect again 
   try {
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.MONGODB_URL) // connect to the DB
     console.log("MongoDB Connected")
   } catch (err: any) {
     console.log("ERROR MESSAGE:", err)
